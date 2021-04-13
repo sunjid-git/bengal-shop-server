f0e85cdfea4f01aff03d8e97d36037a5ffedb89c
sunjid-git <sunjid.info@gmail.com>
2021-04-13 18:02:37 +0600

    finalizing

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/', (req,res) => {
-    res.send('hello from db')
-})
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqs6b.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 console.log(uri);
